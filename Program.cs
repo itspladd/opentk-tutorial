3aac91cc8f3cbfb64be111f820a79e055fb12ee4
Paul Ladd <vlendle@gmail.com>
2023-10-09 22:21:24 -0400

    Initial setup after debugging

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using GameSpace;
+
+namespace OpenTKTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Beans");
+          using (Game game = new Game(800, 600, "LearnOpenTK"))
+          {
+              game.Run();
+          }
+        }
+    }
+}
